7f7d2b5958e93cb4b8315e2976b217ba4b1cf353
Plutossy <pluto_ssy@outlook.com>
2023-11-08 21:34:38 +0800

    完成了分类页面的开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,6 +1,29 @@
 <template>
   <view>
-    Cate
+    <view class="scroll-view-container">
+      <!-- 左侧的滑动区域 -->
+      <scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
+        <block v-for="(item, i) in cateList" :key="i">
+          <view :class="['left-scroll-view-item', i === active ? 'active' : '']" @click="activeChanged(i)">
+            {{item.cat_name}}
+          </view>
+        </block>
+      </scroll-view>
+      <!-- 右侧的滑动区域 -->
+      <scroll-view scroll-y="true" :style="{height: wh + 'px'}" :scroll-top="scrollTop">
+        <view class="cate-lv2" v-for="(item2, i2) in cateList2" :key="i2">
+          <!-- 二级分类标题 -->
+          <view class="cate-lv2-title">&ltcc; {{item2.cat_name}} &gtcc;</view>
+          <!-- 动态渲染三级分类列表 -->
+          <view class="cate-lv3-list">
+            <view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3" @click="gotoGoodsList(item3)">
+              <image :src="item3.cat_icon"></image>
+              <text>{{item3.cat_name}}</text>
+            </view>
+          </view>
+        </view>
+      </scroll-view>
+    </view>
   </view>
 </template>
 
@@ -8,12 +31,119 @@
   export default {
     data() {
       return {
-        
+        // 当前屏幕可使用高度
+        wh: 0,
+        // 分类列表数据
+        cateList: [],
+        // 当前在项
+        active: 0,
+        // 二级分类列表数据
+        cateList2: [],
+        // 滚动条距离顶部的距离
+        scrollTop: 0
       };
+    },
+    onLoad() {
+      // 获取当前屏幕可使用高度
+      const infoSync = uni.getSystemInfoSync()
+      this.wh = infoSync.windowHeight
+
+      // 调用获取分类列表数据的方法
+      this.getCateList()
+    },
+    methods: {
+      // 获取分类列表的方法
+      async getCateList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/categories')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.cateList = res.message
+        // 为二级分类列表赋值
+        this.cateList2 = res.message[0].children
+      },
+
+      // 点击切换导航事件
+      activeChanged(i) {
+        this.active = i
+
+        // 重新获取二级分类列表的数据
+        this.cateList2 = this.cateList[i].children
+
+        // 让 scrollTop 的值在 0 与 0.1 之间切换
+        this.scrollTop = this.scrollTop === 0 ? 0.1 : 0
+      },
+
+      // 点击三级分类跳转到商品列表页面
+      gotoGoodsList(item3) {
+        uni.navigateTo({
+          url: `/subpkg/goods_list/goods_list?cid=${item3.cat_id}`
+        })
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  .scroll-view-container {
+    display: flex;
 
+    .left-scroll-view {
+      width: 120px;
+
+      .left-scroll-view-item {
+        background-color: #F7F7F7;
+        line-height: 60px;
+        text-align: center;
+        font-size: 12px;
+
+        &.active {
+          background-color: #FFFFFF;
+          position: relative;
+
+          &::before {
+            content: '';
+            display: block;
+            width: 3px;
+            height: 30px;
+            background-color: #C00000;
+            position: absolute;
+            top: 50%;
+            left: 0;
+            transform: translateY(-50%);
+          }
+        }
+      }
+    }
+
+    .cate-lv2-title {
+      font-size: 12px;
+      font-weight: bold;
+      text-align: center;
+      padding: 15px 0;
+    }
+
+    .cate-lv3-list {
+      display: flex;
+      flex-wrap: wrap;
+
+      .cate-lv3-item {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 33.33%;
+        margin-bottom: 10px;
+
+        image {
+          width: 60px;
+          height: 60px;
+        }
+
+        text {
+          font-size: 12px;
+        }
+      }
+    }
+  }
 </style>
