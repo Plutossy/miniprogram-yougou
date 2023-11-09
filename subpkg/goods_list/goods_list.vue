eb838637d31e840e3a446a4ca488e4254bb83c43
Plutossy <pluto_ssy@outlook.com>
2023-11-09 17:15:26 +0800

    完成了商品列表的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,9 @@
 <template>
-  <view>
-    goods_list
+  <view class="goods-list">
+    <view v-for="(item, i) in goodsList" :key="i" @click="gotoDetail(item)">
+      <!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
+      <my-goods :goods="item"></my-goods>
+    </view>
   </view>
 </template>
 
@@ -8,12 +11,78 @@
   export default {
     data() {
       return {
-
+        // 请求参数对象
+        queryObj: {
+          // 查询关键词
+          query: '',
+          // 商品分类Id
+          cid: '',
+          // 页码值
+          pagenum: 1,
+          // 每页显示多少条数据
+          pagesize: 10
+        },
+        // 商品列表的数据
+        goodsList: [],
+        // 总数量，用来实现分页
+        total: 0,
+        // 是否正在请求数据
+        isloading: false
       };
+    },
+    onLoad(options) {
+      // 将页面参数转存到 this.queryObj 对象中
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+
+      // 获取商品列表数据
+      this.getGoodsList()
+    },
+    onReachBottom() {
+      // 判断是否还有下一页数据
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+      // 判断是否正在请求其它数据，如果是，则不发起额外的请求
+      if (this.isloading) return
+      // 让页码值自增 +1
+      this.queryObj.pagenum++
+      // 重新获取列表数据
+      this.getGoodsList()
+    },
+    onPullDownRefresh() {
+      // 重置关键数据
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.goodsList = []
+      this.isloading = false
+      // 重新发起请求
+      this.getGoodsList(() => uni.stopPullDownRefresh())
+    },
+    methods: {
+      async getGoodsList(cb) {
+        // 打开节流阀
+        this.isloading = true
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        // 关闭节流阀
+        this.isloading = false
+        // 短路运算 cb存在就调用
+        cb && cb()
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 为数据赋值：通过展开运算符的形式，进行新旧数据的拼接
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+      },
+      // 跳转到商品详情页处理函数
+      gotoDetail(item) {
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+        })
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
+  .goods-list {}
 </style>
